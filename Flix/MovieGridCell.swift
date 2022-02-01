a94ccd5860fefe02bbfc95f15890c0b39724bce4
k4spade <jbeatty@bulldogs.aamu.edu>
2022-01-31 18:10:21 -0600

    Added app files to repository.

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Anthony McGee on 10/30/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
